c6f0acb1fe54d8dbdcd545d3730ff7c2bbeddb46
tbudin <thomasbudin@gmail.com>
2022-02-16 18:24:05 +0100

    Favicon
    Added favicon
    Added OG image

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,8 +13,15 @@ export default function Layout({ children }: Props) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#1b365d" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#ffe196"/>
+        <meta name="apple-mobile-web-app-title" content="MD Consultants"/>
+        <meta name="application-name" content="MD Consultants"/>
+        <meta name="msapplication-TileColor" content="#ffc40d"/>
+        <meta name="theme-color" content="#ffe196"/>
       </Head>
       <header className="site-header">
         <Header />
